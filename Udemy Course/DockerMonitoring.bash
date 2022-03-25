3276d8923f822fcbe3cbe8725abace6c394d0f05
skonienczwy <skonienczwy@hotmail.com>
2022-03-25 12:07:58 +0100

    Delete Udemy Course directory

--- Udemy Course/DockerMonitoring.bash
+++
@@ -1,8 +0,0 @@
-#Check all the process running on Container
-docker container top ContainerID/ContainerName
-
-#Check all details about the Container
-docker container inspect ContainerID/ContainerName
-
-#Check all  live data stream for running containers.
-docker container stats
